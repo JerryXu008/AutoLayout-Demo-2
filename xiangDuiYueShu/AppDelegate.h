bdf0b84ca249d7b3e6d859bea5e0e895af1b18a3
song <song@song-Mac-mini.local>
2015-01-22 15:33:26 +0800

    Initial Commit

---
+++ xiangDuiYueShu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiangDuiYueShu
+//
+//  Created by song on 15/1/22.
+//  Copyright (c) 2015年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiangDuiYueShu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiangDuiYueShu
+//
+//  Created by song on 15/1/22.
+//  Copyright (c) 2015年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
